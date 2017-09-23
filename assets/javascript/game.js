cdb2218f3d8ff6f7cb297c593c2eab9c54ddb866
Joshua Siverson <joshuasiverson@Joshuas-MacBook-Air.local>
2017-09-23 08:38:22 -0700

    create function for assigning value in crystalArr to each image

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,31 +3,39 @@
 var randomNum=0;
 var wins=0;
 var losses=0;
-var cystalArr=[1,];
+var crystalArr=[1];
 
 // ****Functions*****
 
 //resets the game and calls randomNumGen && crystalRandonNum 
 function initalizeGame(){
-  cystalArr=[1];
+  crystalArr=[1];
   randomNum=0;
   randomNumGen();
   crystalRandomNum();
+  assignCrystal();
   console.log(randomNum);
-  console.log(cystalArr);
+  console.log(crystalArr);
 }
 
-// Generates random number between 1-12 and pushes to crystalArr
-function crystalRandomNum(){
-  for (var i = 0; i < 3; i++) {
-    var num= Math.ceil(Math.random() * 12);
-    cystalArr.push(num);
-  }
-}
 
 // Generates random number that will be assigned to the user to guess
 function randomNumGen(){
   randomNum= Math.ceil(Math.random()* (120-30)+ 30);
+}
+// Generates random number between 1-12 and pushes to crystalArr
+function crystalRandomNum(){
+  for (var i = 0; i < 3; i++) {
+    var num= Math.ceil(Math.random() * 12);
+    crystalArr.push(num);
+  }
+}
+
+function assignCrystal(){
+ crystalArr[0] === $("#crystal-1").attr("value", crystalArr[0]);
+ crystalArr[1] === $("#crystal-2").attr("value", crystalArr[1]);
+ crystalArr[2] === $("#crystal-3").attr("value", crystalArr[2]);
+ crystalArr[3] === $("#crystal-4").attr("value", crystalArr[3]);
 }
 
 // ******MAIN GAME*******
